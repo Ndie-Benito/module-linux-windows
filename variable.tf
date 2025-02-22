0a36905580ffec7b2d1a2654ba51dd9c20baffdc
Ndie-Benito <109445998+Ndie-Benito@users.noreply.github.com>
2025-02-21 20:22:16 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -20,5 +20,23 @@ variable "admin_password" {
   sensitive   = true
 }
 
+variable "admin_password" {
+  type        = string
+  description = "Mot de passe administrateur de la VM Windows"
+  sensitive   = true
+}
+
+variable "location" {
+  type        = string
+  default     = "West US 2"
+  description = "Région Azure où les ressources seront déployées"
+}
+
+variable "resource_group_name" {
+  type        = string
+  default     = "terraform-vm"
+  description = "Nom du groupe de ressources Azure"
+}
+
 
 
