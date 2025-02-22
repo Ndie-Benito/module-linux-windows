b776c9600f2506b227d8c44bfc16b2f998bf312b
Ndie-Benito <109445998+Ndie-Benito@users.noreply.github.com>
2025-02-21 20:01:54 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,36 +1,47 @@
 
 
-#  Module VNet : Création du réseau virtuel
+locals {
+  location             = "West US 2"
+  resource_group_name  = "terraform-vm"
+
+  vnet_config = {
+    name = "vnet-module"
+  }
+
+  linux_servers = {
+    os_simple   = "UbuntuServer"
+    public_ip   = ["linsimplevmips"] // Un DNS public unique par région
+  }
+
+  windows_servers = {
+    hostname     = "mywinvm"
+    admin_pass   = "ComplxP@ssw0rd!"
+    os_simple    = "WindowsServer"
+    public_ip    = ["winsimplevmips"]
+  }
+}
+
 module "vnet" {
   source              = "Azure/vnet/azurerm"
   version             = "~> 1.0.0"
-  location            = var.location
-  resource_group_name = var.resource_group_name
+  location            = local.location
+  resource_group_name = local.resource_group_name
 }
-
 
 module "linuxservers" {
   source         = "Azure/vm/azurerm"
-  location       = var.location
-  vm_os_simple   = "UbuntuServer"
-  public_ip_dns  = ["linuxvm-${random_string.suffix.result}"]
+  location       = local.location
+  vm_os_simple   = local.linux_servers.os_simple
+  public_ip_dns  = local.linux_servers.public_ip
   vnet_subnet_id = module.vnet.vnet_subnets[0]
 }
 
-
 module "windowsservers" {
-  source          = "Azure/vm/azurerm"
-  location        = var.location
-  vm_hostname     = "benito-vm"
-  admin_password  = var.admin_password
-  vm_os_simple    = "WindowsServer"
-  public_ip_dns   = ["windowsvm-${random_string.suffix.result}"]
-  vnet_subnet_id  = module.vnet.vnet_subnets[0]
-}
-
-
-resource "random_string" "suffix" {
-  length  = 6
-  special = false
-  upper   = false
+  source         = "Azure/vm/azurerm"
+  location       = local.location
+  vm_hostname    = local.windows_servers.hostname
+  admin_password = local.windows_servers.admin_pass
+  vm_os_simple   = local.windows_servers.os_simple
+  public_ip_dns  = local.windows_servers.public_ip
+  vnet_subnet_id = module.vnet.vnet_subnets[0]
 }
