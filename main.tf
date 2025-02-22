2f77ffa803285d686515f1253b369a426fff21c9
Ndie-Benito <109445998+Ndie-Benito@users.noreply.github.com>
2025-02-21 20:27:49 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,37 +1,26 @@
-locals {
-  linux_servers = {
-    os_simple  = "UbuntuServer"
-    public_ip  = ["linsimplevmips"] // DNS public unique par région
-  }
-
-  windows_servers = {
-    hostname   = "benito-vm"
-    os_simple  = "WindowsServer"
-    public_ip  = ["winsimplevmips"]
-  }
-}
-
 module "vnet" {
-  source              = "Azure/vnet/azurerm"
-  version             = "~> 1.0.0"
+  source              = "Azure/network/azurerm"
+  version             = "~> 3.0.0"
   location            = var.location
   resource_group_name = var.resource_group_name
 }
 
 module "linuxservers" {
-  source         = "Azure/vm/azurerm"
+  source         = "Azure/compute/azurerm"
+  version        = "~> 3.0.0"
   location       = var.location
-  vm_os_simple   = local.linux_servers.os_simple
-  public_ip_dns  = local.linux_servers.public_ip
+  vm_os_simple   = "UbuntuServer"
+  public_ip_dns  = ["linsimplevmips"]
   vnet_subnet_id = module.vnet.vnet_subnets[0]
 }
 
 module "windowsservers" {
-  source         = "Azure/vm/azurerm"
+  source         = "Azure/compute/azurerm"
+  version        = "~> 3.0.0"
   location       = var.location
-  vm_hostname    = local.windows_servers.hostname
-  admin_password = var.admin_password  # Utilisation de la variable sensible ici
-  vm_os_simple   = local.windows_servers.os_simple
-  public_ip_dns  = local.windows_servers.public_ip
+  vm_hostname    = "mywinvm"
+  admin_password = var.admin_password
+  vm_os_simple   = "WindowsServer"
+  public_ip_dns  = ["winsimplevmips"]
   vnet_subnet_id = module.vnet.vnet_subnets[0]
 }
